40ba9c61454827a8c61337ea7180b4d97c71620f
jin-summer <kj0829@gmail.com>
2020-06-23 17:16:31 +0900

    create user login form and arrange UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :new, :edit, :create, :update]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :posts
   root 'top#index'
+  match 'signup', to: 'users#new', via: 'get'
+  match 'signin', to: 'sessions#new', via: 'get'
+  match 'signout', to: 'sessions#destroy', via: 'delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
