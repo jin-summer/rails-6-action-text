effc844a094672cfbff7792444a028f79028b32f
jin-summer <kj0829@gmail.com>
2020-06-24 16:40:05 +0900

    current_user to current

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   def ensure_correct_user
     @post = Post.find_by(id: params[:id])
-    if @post.user_id != @current_user.id
+    if @post.user_id != @current.id
     flash[:notice] = "権限がありません"
     redirect_to("/posts")
     end
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
     @post = Post.new(
       post_params
       )
-    @post.user_id = @current_user.id
+    @post.user_id = @current.id
 
     respond_to do |format|
       if @post.save
